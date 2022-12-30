b7ff2592a396f62d88424546c1183536edcb1dcd
Michael Brockman <mbrockmandev@gmail.com>
2022-12-30 15:02:03 -0500

    feat: add prev and next buttons. add basic model. left off at beginning of ch3.

--- geoQuiz/app/src/main/java/com/mbdev/geoquiz/MainActivity.kt
+++ geoQuiz/app/src/main/java/com/mbdev/geoquiz/MainActivity.kt
@@ -2,34 +2,58 @@ package com.mbdev.geoquiz
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
-import android.widget.Toast
+import com.google.android.material.snackbar.Snackbar
+import com.mbdev.geoquiz.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var trueButton: Button
-    private lateinit var falseButton: Button
+    private lateinit var binding: ActivityMainBinding
+
+    private val questionBank = listOf(
+        Question(R.string.question_australia, true),
+        Question(R.string.question_oceans, true),
+        Question(R.string.question_mideast, true),
+        Question(R.string.question_africa, true),
+        Question(R.string.question_americas, true),
+        Question(R.string.question_asia, true),
+    )
+    private var currentIndex = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        trueButton = findViewById(R.id.true_button)
-        falseButton = findViewById(R.id.false_button)
-
-        trueButton.setOnClickListener {
-            Toast.makeText(
-                this,
-                R.string.correct_toast,
-                Toast.LENGTH_SHORT
-            ).show()
+        binding.btnTrue.setOnClickListener {
+            checkAnswer(true)
         }
 
-        falseButton.setOnClickListener {
-            Toast.makeText(
-                this,
-                R.string.incorrect_toast,
-                Toast.LENGTH_SHORT
-            ).show()
+        binding.btnFalse.setOnClickListener {
+            checkAnswer(false)
         }
+
+        binding.btnNext.setOnClickListener {
+            currentIndex = (currentIndex + 1) % questionBank.size
+            updateQuestion()
+        }
+
+        updateQuestion()
+
+    }
+
+    private fun updateQuestion() {
+        val questionTextResId = questionBank[currentIndex].textResId
+        binding.tvQuestion.setText(questionTextResId)
+    }
+
+    private fun checkAnswer(userAnswer: Boolean) {
+        val correctAnswer = questionBank[currentIndex].answer
+
+        val messageResId = if (userAnswer == correctAnswer) {
+            R.string.correct_snackbar
+        } else {
+            R.string.incorrect_snackbar
+        }
+
+        Snackbar.make(binding.llMain, messageResId, Snackbar.LENGTH_SHORT).show()
     }
 }

6ae644844dc53c5b2f9caffa59cc07c07e747fc6
Michael Brockman <mbrockmandev@gmail.com>
2023-01-06 17:16:12 -0500

    start ch10

--- CriminalIntent/app/src/main/java/com/mbdev/criminalintent/CrimeDetailFragment.kt
+++ CriminalIntent/app/src/main/java/com/mbdev/criminalintent/CrimeDetailFragment.kt
@@ -10,8 +10,12 @@ import com.mbdev.criminalintent.databinding.FragmentCrimeDetailBinding
 import java.util.*
 
 class CrimeDetailFragment : Fragment() {
-    private lateinit var binding: FragmentCrimeDetailBinding
-    private lateinit var crime: Crime
+    lateinit var crime: Crime
+    private var _binding: FragmentCrimeDetailBinding? = null
+    private val binding
+        get() = checkNotNull(_binding) {
+            "Cannot access binding because it is null. Is the view visible?"
+        }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -29,7 +33,7 @@ class CrimeDetailFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        binding = FragmentCrimeDetailBinding.inflate(layoutInflater, container, false)
+        _binding = FragmentCrimeDetailBinding.inflate(layoutInflater, container, false)
         return binding.root
     }
 
@@ -39,18 +43,19 @@ class CrimeDetailFragment : Fragment() {
             etCrimeTitle.doOnTextChanged { text, _, _, _ ->
                 crime = crime.copy(title = text.toString())
             }
-
             btnCrimeDate.apply {
                 text = crime.date.toString()
                 isEnabled = false
             }
-
             cbCrimeSolved.setOnCheckedChangeListener { _, isChecked ->
                 crime = crime.copy(isSolved = isChecked)
             }
-
-
         }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 
 }
---
+++ CriminalIntent/app/src/test/java/com/mbdev/criminalintent/CrimeDetailFragmentTest.kt
@@ -0,0 +1,21 @@
+package com.mbdev.criminalintent
+
+import androidx.fragment.app.testing.launchFragmentInContainer
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import org.junit.Assert.*
+
+
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+class CrimeDetailFragmentTest {
+//
+//    @Test
+//    fun testEventFragment() {
+//        val scenario = launchFragmentInContainer<CrimeDetailFragment>()
+//        onView(withId(R.id.fcvCrimeDetail)).perform(click())
+//    }
+
+
+}

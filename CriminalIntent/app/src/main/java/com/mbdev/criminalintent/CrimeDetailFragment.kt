df79b015b6532126feac44169c1822b260d29624
Michael Brockman <mbrockmandev@gmail.com>
2023-01-06 16:19:24 -0500

    feat: add crime detail fragment layout and basic kt fragment

---
+++ CriminalIntent/app/src/main/java/com/mbdev/criminalintent/CrimeDetailFragment.kt
@@ -0,0 +1,35 @@
+package com.mbdev.criminalintent
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.mbdev.criminalintent.databinding.FragmentCrimeDetailBinding
+import java.util.*
+
+class CrimeDetailFragment : Fragment() {
+    private lateinit var binding: FragmentCrimeDetailBinding
+    private lateinit var crime: Crime
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        crime = Crime(
+            id = UUID.randomUUID(),
+            title = "",
+            date = Date(),
+            isSolved = false
+        )
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentCrimeDetailBinding.inflate(layoutInflater, container, false)
+        return binding.root
+    }
+
+}

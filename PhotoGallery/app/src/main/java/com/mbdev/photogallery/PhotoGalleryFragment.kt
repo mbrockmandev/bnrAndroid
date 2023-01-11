2eb0720378e2adbf52412934c268cd115036f767
Michael Brockman <mbrockmandev@gmail.com>
2023-01-10 19:41:06 -0500

    ch20 start of networking with retrofit

---
+++ PhotoGallery/app/src/main/java/com/mbdev/photogallery/PhotoGalleryFragment.kt
@@ -0,0 +1,51 @@
+package com.mbdev.photogallery
+
+import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.lifecycleScope
+import androidx.recyclerview.widget.GridLayoutManager
+import com.mbdev.photogallery.api.FlickrApi
+import com.mbdev.photogallery.databinding.FragmentPhotoGalleryBinding
+import kotlinx.coroutines.launch
+import retrofit2.Retrofit
+import retrofit2.converter.scalars.ScalarsConverterFactory
+import retrofit2.create
+
+private const val TAG = "PhotoGalleryFragment"
+
+class PhotoGalleryFragment: Fragment() {
+    private var _binding: FragmentPhotoGalleryBinding? = null
+    private val binding
+        get() = checkNotNull(_binding) {
+            "Cannot access binding because it is null. Is the view visible?"
+        }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentPhotoGalleryBinding.inflate(layoutInflater, container, false)
+        binding.rvPhotoGrid.layoutManager = GridLayoutManager(context, 3)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        viewLifecycleOwner.lifecycleScope.launch {
+            val response = PhotoRepository().fetchContents()
+            Log.d(TAG, "Response received: $response")
+        }
+
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}

289bd982cf7d8af3402f3552577a34c65974b97e
Michael Brockman <mbrockmandev@gmail.com>
2023-01-17 13:35:17 -0500

    tbd reversible sunset animation on click

--- Sunset/app/src/main/java/com/example/sunset/MainActivity.kt
+++ Sunset/app/src/main/java/com/example/sunset/MainActivity.kt
@@ -5,12 +5,15 @@ import android.animation.ArgbEvaluator
 import android.animation.ObjectAnimator
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.animation.AccelerateInterpolator
 import androidx.core.content.ContextCompat
 import com.example.sunset.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+
+    private val animatorSet = AnimatorSet()
 
     private val blueSkyColor: Int by lazy {
         ContextCompat.getColor(this, R.color.blue_sky)
@@ -28,11 +31,24 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         binding.scene.setOnClickListener {
-            startAnimation()
+            animateSun()
         }
     }
 
-    private fun startAnimation() {
+    private fun animateSun() {
+        Log.d("MainActivity>>>", "sun top = ")
+        Log.d("MainActivity>>>", binding.sun.top.toString())
+        Log.d("MainActivity>>>", "sky height = ")
+        Log.d("MainActivity>>>", binding.sky.height.toString())
+
+        if (animatorSet.isRunning) {
+            animateSunDown()
+        } else {
+            animateSunUp()
+        }
+    }
+
+    private fun animateSunDown() {
         val sunYStart = binding.sun.top.toFloat()
         val sunYEnd = binding.sky.height.toFloat()
 
@@ -51,10 +67,36 @@ class MainActivity : AppCompatActivity() {
             .setDuration(1500)
         nightSkyAnimator.setEvaluator(ArgbEvaluator())
 
-        val animatorSet = AnimatorSet()
+
         animatorSet.play(heightAnimator)
             .with(sunsetSkyAnimator)
             .before(nightSkyAnimator)
+        animatorSet.start()
+    }
+
+    private fun animateSunUp() {
+        val sunYStart = binding.sun.top.toFloat()
+        val sunYEnd = binding.sky.height.toFloat()
+
+        val heightAnimator = ObjectAnimator
+            .ofFloat(binding.sun, "y", sunYEnd, sunYStart)
+            .setDuration(3000)
+        heightAnimator.interpolator = AccelerateInterpolator()
+
+        val sunsetSkyAnimator = ObjectAnimator
+            .ofInt(binding.sky, "backgroundColor", sunsetSkyColor, blueSkyColor)
+            .setDuration(3000)
+        sunsetSkyAnimator.setEvaluator(ArgbEvaluator())
+
+        val nightSkyAnimator = ObjectAnimator
+            .ofInt(binding.sky, "backgroundColor", nightSkyColor, sunsetSkyColor)
+            .setDuration(1500)
+        nightSkyAnimator.setEvaluator(ArgbEvaluator())
+
+        val animatorSet = AnimatorSet()
+        animatorSet.play(sunsetSkyAnimator)
+            .before(nightSkyAnimator)
+            .with(heightAnimator)
         animatorSet.start()
     }
 }

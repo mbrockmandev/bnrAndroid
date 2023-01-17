7381e8947f90b15c6ad1be73d7507c3e635808a6
Michael Brockman <mbrockmandev@gmail.com>
2023-01-17 13:10:37 -0500

    add sunset animation

--- Sunset/app/src/main/java/com/example/sunset/MainActivity.kt
+++ Sunset/app/src/main/java/com/example/sunset/MainActivity.kt
@@ -1,11 +1,32 @@
 package com.example.sunset
 
+import android.animation.ObjectAnimator
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.animation.AccelerateInterpolator
+import com.example.sunset.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.scene.setOnClickListener {
+            startAnimation()
+        }
+    }
+
+    private fun startAnimation() {
+        val sunYStart = binding.sun.top.toFloat()
+        val sunYEnd = binding.sky.height.toFloat()
+
+        val heightAnimator = ObjectAnimator
+            .ofFloat(binding.sun, "y", sunYStart, sunYEnd)
+            .setDuration(3000)
+        heightAnimator.interpolator = AccelerateInterpolator()
+
+        heightAnimator.start()
     }
 }
